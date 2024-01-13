1ed918252b47be152f8e98fdeaec709c70f4bd48
muzili <2586850402@qq.com>
2024-01-13 23:33:26 +0800

    开头install方法, 补充getNoteinfo

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -3,18 +3,33 @@
 namespace app\controller;
 
 use app\BaseController;
+use app\model\User;
 
 class Index extends Login
 {
 
     public function getNoteInfo()
     {
-        //检查登陆状态
-        return json(array("code" => 1, "goto"=>url("page/login")->build()));    // 1:未登录
-
+        //检查安装状态
+        $user = new User();
+        if(!$user->count()){
+            return json(array("code" => 2, "goto"=>url("page/install")->build()));    // 2:未安装
+        }
+        if(!LOGIN){
+            //检查登陆状态
+            return json(array("code" => 1, "goto"=>url("page/login")->build()));    // 1:未登录
+        }
+        //TODO: 已登录
 
     }
 
+    public function install()
+    {
+        // 返回code：0 成功,msg显示提示消息，跳转主页 ，1 失败,显示msg消息
+        if((new User())->count()){
+            // TODO: 返回已安装
+        }
+    }
 
     public function index()
     {
