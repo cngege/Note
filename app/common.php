acaa93476d6c8dc61eb819c6ef7ad5329fd8388e
muzili <2586850402@qq.com>
2024-01-23 15:38:11 +0800

    添加getuser方法，限制密码等信息传回前端

--- app/common.php
+++ app/common.php
@@ -22,3 +22,34 @@ function thinkUcenterMd5($str, $key = 'ThinkUCenter')
 {
     return '' === $str ? '' : md5(sha1($str) . $key);
 }
+
+/**
+ * Desc: 尽可能的拿到能够在前端显示的数据
+ * @param int $id   主键id
+ * @return array    拿到的用户信息数据
+ * @throws \think\db\exception\DataNotFoundException
+ * @throws \think\db\exception\DbException
+ * @throws \think\db\exception\ModelNotFoundException
+ */
+function getUser(int $id)
+{
+    $user = app\Model\User::find($id);
+    if(!$user){
+        return array();
+    }
+    $ret = array();
+    $ret["id"] = $user->id;
+    $ret["nickname"] = $user->nickname;
+    $ret["email"] = $user->email;
+    $ret["phone"] = $user->phone;
+    if(is_null($user["userface"])){
+        $ret["userface"] = "/static/img/akari.jpg";//默认图片url
+    }else{
+        $ret["userface"] = think\facade\Filesystem::disk("userData")->url($user["userface"]);
+    }
+    $ret["is_admin"] = $user->is_admin;
+    $ret["status"] = $user->status;
+    $ret["create_time"] = $user->create_time;
+    $ret["update_time"] = $user->update_time;
+    return $ret;
+}
