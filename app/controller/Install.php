2860416d919181e91d5d9a015efe0b8d5b9118a6
muzili <2586850402@qq.com>
2024-01-25 22:33:45 +0800

    安装时添加管理用户关联的一张笔记文件夹表

--- app/controller/Install.php
+++ app/controller/Install.php
@@ -12,6 +12,10 @@ namespace app\controller;
 
 use app\BaseController;
 use app\model\User;
+use app\model\Folder;
+use think\facade\Db;
+
+use Ramsey\Uuid\Uuid;
 
 class Install extends BaseController
 {
@@ -27,18 +31,46 @@ class Install extends BaseController
         $data = input("post.");
         $salt = mt_randStr();
         if(!empty($data["adminuser"] && !empty($data["adminpasswd"]))){
+            $uuid = UUID::uuid4()->toString();
             $InsData = [
                 "nickname" => $data["adminuser"],
                 "password" => thinkUcenterMd5($data["adminpasswd"],$salt),
                 "salt"     => $salt,
                 "reg_ip" => request()->ip(),
                 "login_ip" => request()->ip(),
+                "folder_uuid" => $uuid,
                 "is_admin" => 1,
             ];
-            if($user->save($InsData)){
-                session('login_auth', $user->getData());
-                return json(["code"=>0,"msg"=>"安装完成","goto"=>url("/index")->build()]);
+            Db::startTrans();
+            try {
+                if($user->save($InsData)){
+                    /* 向 Folder 模型中写入默认文件夹结构 */
+                    $folder = new Folder();
+                    // 编写一条数据 笔记文件夹下 我的笔记 这个文件夹的uuid
+                    $mynote_uuid = UUID::uuid4()->toString();
+                    $folderData = [
+                        "uid" => $user["id"],
+                        "uuid"=> $uuid,
+                        "name"=> "根目录",
+                        "subfolder" => json_encode([
+                            ["uuid"=>$mynote_uuid, "name"=>"我的笔记"],
+                        ]),
+                    ];
+                    if(!$folder->save($folderData)){
+                        // 创建默认笔记文件夹（保存）失败
+                        Db::rollback();
+                        return json(["code"=>1,"msg"=>"安装失败"]);
+                    }
+                    Db::commit();
+                    // 创建用户和创建默认文件夹都成功
+                    session('login_auth', $user->getData());
+                    return json(["code"=>0,"msg"=>"安装完成","goto"=>url("/index")->build()]);
+                }
+            }catch (\Exception $e){
+                Db::rollback();
+                return json(["code"=>1,"msg"=>"安装失败","error"=>$e->getMessage()]);
             }
+
             return json(["code"=>1,"msg"=>"安装失败"]);
         }
         return json(["code"=>1,"msg"=>"用户名或密码不能为空"]);
