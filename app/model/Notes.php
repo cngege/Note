c8ed146f597b2aad502a712bfa8b56bf66109c55
muzili <2586850402@qq.com>
2024-01-25 22:30:06 +0800

    添加 Folder Nodes 模型

---
+++ app/model/Notes.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: CNGEGE
+ * Date: 2024/1/25
+ * Time: 16:53
+ * Brief:
+ * docs:
+ */
+
+namespace app\model;
+
+use think\Model;
+
+class Notes extends Model
+{
+
+}
