ac872d15f2af956b8596c920a442a69b592b6940
muzili <2586850402@qq.com>
2024-01-29 16:00:52 +0800

    获取文件夹list和获取笔记接口分开

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -26,9 +26,14 @@ class Index extends Login
         return json(["code"=>0,"user"=>getUser(UID)]);
     }
 
-
-
-    public function getNoteInfo()   // 获取某个层级文件夹下的子级文件夹和笔记id 1 没有登录 2 出错
+    /**
+     * Desc: 取某个文件夹下的下一级所有文件夹
+     * @return \think\response\Json 接口回传消息
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function getFolderInfo()   // 获取某个层级文件夹下的子级文件夹 1 没有登录 2 出错
     {
         if(!LOGIN){
             //检查登陆状态
@@ -36,7 +41,7 @@ class Index extends Login
         }
         $uuid = input("post.uuid");
         if(!$uuid){
-            return json(["code"=>2,"msg"=>"参数错误", "folder" => [], "note"=>[]]);
+            return json(["code"=>2,"msg"=>"参数错误", "folder" => []]);
         }
         $user = User::find(UID);
         $folders = $user
@@ -45,13 +50,34 @@ class Index extends Login
             ->field("parent_uuid,uuid,name,subfolder")
             ->find();// 理论只有一条数据，所有这里用find，否则用select
 
+        return json(["code"=>0,"folder"=>$folders]);
+    }
+
+    /**
+     * Desc: 获取某个文件夹下的下一级所有笔记文件
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function getNoteInfo()   // 获取某个层级文件夹下的子级文件夹和笔记id 1 没有登录 2 出错
+    {
+        if(!LOGIN){
+            //检查登陆状态
+            return json(array("code" => 1, "goto"=>url("page/login")->build()));    // 1:未登录
+        }
+        $uuid = input("post.uuid");
+        if(!$uuid){
+            return json(["code"=>2,"msg"=>"参数错误", "note"=>[]]);
+        }
+        $user = User::find(UID);
         $notes = $user
             ->Notes()
             ->where("parent_uuid", $uuid)
             ->field("parent_uuid,uuid,title")
             ->select();
 
-        return json(["code"=>0,"folder"=>$folders,"note"=>(count($notes))? array($notes) : array()]);
+        return json(["code"=>0,"note"=>(count($notes))? array($notes) : array()]);
     }
 
 }
