f3117f55d7adda12b58180f11da876523ecf4143
muzili <2586850402@qq.com>
2024-02-01 00:30:14 +0800

    表结构更新同步

--- delMe/note.sql
+++ delMe/note.sql
@@ -14,7 +14,43 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+-- 导出  表 note.note_folder 结构
+DROP TABLE IF EXISTS `note_folder`;
+CREATE TABLE IF NOT EXISTS `note_folder` (
+  `uid` int unsigned NOT NULL COMMENT '所属用户id',
+  `parent_uuid` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '父文件的uuid（当前文件夹为根文件夹时，父文件夹uuid为null）',
+  `uuid` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '当前文件夹的uuid',
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '当前文件夹名称',
+  `subfolder` json NOT NULL COMMENT '所有下一级子文件夹',
+  UNIQUE KEY `uuid` (`uuid`),
+  KEY `FK_note_folder_note_user` (`uid`),
+  CONSTRAINT `FK_note_folder_note_user` FOREIGN KEY (`uid`) REFERENCES `note_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='笔记文件夹结构表';
+
+-- 数据导出被取消选择。
+
+-- 导出  表 note.note_notes 结构
+DROP TABLE IF EXISTS `note_notes`;
+CREATE TABLE IF NOT EXISTS `note_notes` (
+  `uid` int unsigned NOT NULL COMMENT '所属用户id',
+  `parent_uuid` varchar(40) COLLATE utf8mb4_general_ci NOT NULL COMMENT '父文件夹uuid',
+  `uuid` varchar(40) COLLATE utf8mb4_general_ci NOT NULL COMMENT '笔记uuid',
+  `title` varchar(100) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '未命名' COMMENT '笔记标题',
+  `filename` varchar(40) COLLATE utf8mb4_general_ci NOT NULL COMMENT '笔记文件名称',
+  `description` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '笔记内容的前50个字',
+  `img` json DEFAULT NULL COMMENT '笔记当中的图片资源',
+  `remark` json DEFAULT NULL COMMENT '备注',
+  `attachment` json DEFAULT NULL COMMENT '附件名单',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '笔记创建时间',
+  UNIQUE KEY `uuid` (`uuid`),
+  KEY `FK_note_notes_note_user` (`uid`),
+  CONSTRAINT `FK_note_notes_note_user` FOREIGN KEY (`uid`) REFERENCES `note_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='笔记文件存储信息表';
+
+-- 数据导出被取消选择。
+
 -- 导出  表 note.note_user 结构
+DROP TABLE IF EXISTS `note_user`;
 CREATE TABLE IF NOT EXISTS `note_user` (
   `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '用户id',
   `nickname` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名称',
@@ -25,13 +61,14 @@ CREATE TABLE IF NOT EXISTS `note_user` (
   `userface` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '头像URL',
   `reg_ip` varchar(35) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '创建账号时的IP',
   `login_ip` varchar(35) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '最新一次登陆的IP',
+  `folder_uuid` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '笔记根文件夹的uuid',
   `is_admin` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0普通用户,1管理员',
   `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0正常,1禁用2拉黑',
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '用户创建时间',
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新信息的更新时间',
   `delete_time` timestamp NULL DEFAULT NULL COMMENT '软删除',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='基础用户账号信息表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='基础用户账号信息表';
 
 -- 数据导出被取消选择。
 
