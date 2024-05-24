6259aea92fc523a0dce0b7f0cf4f539186700b34
muzili <2586850402@qq.com>
2024-05-24 22:20:28 +0800

    修复 弹出菜单的onClose事件的调用问题

--- public/static/script/popup-win.js
+++ public/static/script/popup-win.js
@@ -5,15 +5,13 @@ $(".popup-win_div").each(function(index, el) {
   let close = $("<div class=\"popup_win_close\"></div>");
   close.click(function(event) {
     $(this).parent().hide();
+    let onClose = $(this).parent().attr("onClose");
+    if(onClose !== undefined){
+      eval(onClose);
+    }
   });
   close.on('contextmenu',e=>e.preventDefault())
   $(el).prepend(close);
-
-  let onClose = $(el).attr("onClose");
-  if(onClose !== undefined){
-    eval(onClose);
-  }
-
 });
 
 })
